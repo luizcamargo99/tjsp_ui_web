620625614fc4339882519bf2d9d4ea6496b830cf
Luiz Camargo <luiz.zito@hotmail.com>
2022-03-14 19:39:32 -0300

    ajustes card noticias

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,18 +1,49 @@
+
 const imagens = [
-    './Images/banner1.png',
-    './Images/banner2.png',
-    './Images/banner3.jpg',
-    './Images/banner4.jpg'
+    {'url': './Images/banner1.png',
+    'titulo': 'Evento on-line da CIJ aborda maternidade e paternidade biológicas e adotivas'},
+    {'url': './Images/banner2.png',
+    'titulo': 'Inscrições abertas para o 19º Prêmio Innovare'},
+    {'url': './Images/banner3.jpg',
+    'titulo': 'Mulher que teve nome incluído em lista de suspeitos de irregularidades...'},
+    {'url': './Images/banner4.jpg',
+    'titulo': 'Projetos do TJSP promovem conciliação na iniciativa privada e no Poder Público'},
 ];
 
 let countBannerNoticias = 0;
 
 function carregamentoInicial () {
-    bannerNoticiasLoop();
+    criarCardsNoticias();
+    selecionarImagem(0);
+    bannerNoticiasLoop(); 
 }
 
-function bannerNoticiasLoop () {
-    definirImagemSrc(imagens[0]);
+function criarCardsNoticias () {
+   for (let index = 0; index < imagens.length; index++) {
+
+       const divCard = document.createElement("div");
+       divCard.id = `card-banner${index.toString()}`;
+       divCard.classList.add('card', 'card-noticia');
+
+       divCard.addEventListener('click', function() {
+        selecionarImagem(index);
+      });
+
+       if (index == 0){
+        divCard.classList.add('active');
+       }
+
+       const textoCard = document.createElement('p');
+       textoCard.innerHTML = imagens[index].titulo;
+
+       divCard.appendChild(textoCard);      
+
+       const divNoticias = document.getElementById('cards-noticias');
+       divNoticias.appendChild(divCard);
+   }
+}
+
+function bannerNoticiasLoop () {    
     setInterval(() => {  
         selecionarImagem(countBannerNoticias+1);
     }, 5000);    
@@ -24,7 +55,7 @@ function validarLimiteCarrossel () {
     }
 }
 
-function definirImagemAtiva () {
+function definirCardAtivo () {
     const cardAtivo = document.getElementById('card-banner' + countBannerNoticias.toString());
     cardAtivo.classList.add('active');
 }
@@ -33,8 +64,7 @@ function removerCardsAtivos () {
     const cards = document.getElementsByClassName('card-noticia');
     for (let index = 0; index < cards.length; index++) {
         const card = cards[index];
-        card.classList.remove('active');
-        
+        card.classList.remove('active');        
     }
 }
 
@@ -42,13 +72,19 @@ function selecionarImagem (numeroImagem){
     countBannerNoticias = numeroImagem;
     validarLimiteCarrossel();
     removerCardsAtivos();
-    definirImagemAtiva();
-    definirImagemSrc(imagens[countBannerNoticias]);
+    definirCardAtivo();    
+    definirImagemSrc(imagens[countBannerNoticias].url);
+    definirCaptionImagem(imagens[countBannerNoticias].titulo);
 }
 
 function definirImagemSrc(caminhoImagem){
     const bannerImagem = document.getElementById('banner-noticia');   
     bannerImagem.src = caminhoImagem;  
+}
+
+function definirCaptionImagem(titulo){
+    const captionTexto = document.getElementById('caption-noticia-texto');   
+    captionTexto.innerHTML = titulo; 
 }
 
 function voltarImagem (){
